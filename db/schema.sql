b3bdb0f0fbf412a08ec65b6bb15ab533649009a2
Sky <erniesanchez619@gmial.com>
2019-09-26 10:06:58 -0700

    created schema file inside the db folder

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured boolean NOT NULL,
+	PRIMARY KEY (id)
+);
