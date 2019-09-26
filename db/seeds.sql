3d3f4f0174a9aba29e621b506d649002ac92ae3a
Sky <erniesanchez619@gmial.com>
2019-09-26 10:16:52 -0700

    created database in mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burger (burger_name, devoured) 
+INSERT INTO burgers (burger_name, devoured) 
 VALUES ('spaghetti Burger', true),
 	   ('Chicken Burger', false),
 	   ('Taco Burger', false);
